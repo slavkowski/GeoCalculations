f7190e6d1beac721b4d55f4ec2901bc0046cdb41
Slaptop\Slavkowski <szweds@poczta.onet.pl>
2019-11-05 22:05:44 +0100

    introduce File Upload Error Response

--- src/main/java/pl/sats/FileUtils/FileUtils.java
+++ src/main/java/pl/sats/FileUtils/FileUtils.java
@@ -4,8 +4,7 @@ import pl.sats.FieldObservationsObjects.DeltaHeight;
 import pl.sats.FieldObservationsObjects.PointCoordinates.*;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static pl.sats.FieldObservationsObjects.PointCoordinates.PointType.LHE;
 
@@ -22,6 +21,7 @@ public class FileUtils<T extends Point> {
     private List<T> list;
     private T t;
     private PointType pointType;
+    private ReadFileResponse readFileResponse;
 
     public FileUtils() {
     }
@@ -33,85 +33,112 @@ public class FileUtils<T extends Point> {
     }
 
     public List<T> readFile(File file) throws IOException {
+        readFileResponse = new ReadFileResponse();
         List<T> returnList = new ArrayList<>();
         String line;
         FileReader fr = new FileReader(file);
         int elementsInOneLine = identifyNumberOfElements(pointType);
+
+        int numberOfLineInFile = 0;
+        int correctNumberOfLinesInFile = 0;
+        Map<Integer, String> wrongNumberOfArgumentsInLine = new HashMap<>();
+        Set<Integer> numberFormatException = new HashSet<>();
+
         try (BufferedReader f = new BufferedReader(fr)) {
             while ((line = f.readLine()) != null) {
+                numberOfLineInFile++;
                 String[] splitLine = line.split(",");
                 if (splitLine.length == elementsInOneLine) {
-                    if (t instanceof LHD) {
-                        switch (pointType) {
-                            case LH:
-                                returnList.add((T) new LHD(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), 0.0));
-                                break;
-                            case LHD:
-                                returnList.add((T) new LHD(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3])));
-                                break;
-                            case LHDE:
-                                returnList.add((T) new LHD(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]), Double.parseDouble(splitLine[4]), Double.parseDouble(splitLine[5]), Double.parseDouble(splitLine[6])));
-                                break;
-                            case LHE:
-                                returnList.add((T) new LHD(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]), Double.parseDouble(splitLine[4]), 0.0, 0.0));
-                                break;
+                    try {
+                        correctNumberOfLinesInFile++;
+                        if (t instanceof LHD) {
+                            switch (pointType) {
+                                case LH:
+                                    LHD lhd_1 = new LHD(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), 0.0);
+                                    returnList.add((T) lhd_1);
+                                    break;
+                                case LHD:
+                                    LHD lhd_2 = new LHD(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]));
+                                    returnList.add((T) lhd_2);
+                                    break;
+                                case LHDE:
+                                    LHD lhd_3 = new LHD(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]), Double.parseDouble(splitLine[4]), Double.parseDouble(splitLine[5]), Double.parseDouble(splitLine[6]));
+                                    returnList.add((T) lhd_3);
+                                    break;
+                                case LHE:
+                                    LHD lhd_4 = new LHD(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]), Double.parseDouble(splitLine[4]), 0.0, 0.0);
+                                    returnList.add((T) lhd_4);
+                                    break;
+                            }
+                        } else if (t instanceof XYZ) {
+                            switch (pointType) {
+                                case XYZ:
+                                    XYZ xyz_1 = new XYZ(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]));
+                                    returnList.add((T) xyz_1);
+                                    break;
+                                case XYZE:
+                                    XYZ xyz_2 = new XYZ(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]), Double.parseDouble(splitLine[4]), Double.parseDouble(splitLine[5]), Double.parseDouble(splitLine[6]));
+                                    returnList.add((T) xyz_2);
+                                    break;
+                            }
+                        } else if (t instanceof BLH) {
+                            switch (pointType) {
+                                case BLH:
+                                    BLH blh_1 = new BLH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]));
+                                    returnList.add((T) blh_1);
+                                    break;
+                                case BLHE:
+                                    BLH blh_2 = new BLH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]), Double.parseDouble(splitLine[4]), Double.parseDouble(splitLine[5]), Double.parseDouble(splitLine[6]));
+                                    returnList.add((T) blh_2);
+                                    break;
+                            }
+                        } else if (t instanceof NEH) {
+                            switch (pointType) {
+                                case NE:
+                                    NEH neh_1 = new NEH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]));
+                                    returnList.add((T) neh_1);
+                                    break;
+                                case NEE:
+                                    NEH neh_2 = new NEH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[3]));
+                                    neh_2.setmN(Double.parseDouble(splitLine[2]));
+                                    neh_2.setmE(Double.parseDouble(splitLine[4]));
+                                    returnList.add((T) neh_2);
+                                    break;
+                                case NEH:
+                                    NEH neh_3 = new NEH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]));
+                                    returnList.add((T) neh_3);
+                                    break;
+                                case NEHE:
+                                    NEH neh_4 = new NEH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[3]), Double.parseDouble(splitLine[5]));
+                                    neh_4.setmN(Double.parseDouble(splitLine[2]));
+                                    neh_4.setmE(Double.parseDouble(splitLine[4]));
+                                    neh_4.setmH(Double.parseDouble(splitLine[6]));
+                                    returnList.add((T) neh_4);
+                                    break;
+                                case H:
+                                    NEH neh_5 = new NEH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]));
+                                    returnList.add((T) neh_5);
+                                    break;
+                                case HE:
+                                    NEH neh_6 = new NEH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]));
+                                    neh_6.setmH(Double.parseDouble(splitLine[2]));
+                                    returnList.add((T) neh_6);
+                                    break;
+                            }
                         }
-                    } else if (t instanceof XYZ) {
-                        switch (pointType) {
-                            case XYZ:
-                                returnList.add((T) new XYZ(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3])));
-                                break;
-                            case XYZE:
-                                returnList.add((T) new XYZ(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]), Double.parseDouble(splitLine[4]), Double.parseDouble(splitLine[5]), Double.parseDouble(splitLine[6])));
-                                break;
-                        }
-                    } else if (t instanceof BLH) {
-                        switch (pointType) {
-                            case BLH:
-                                returnList.add((T) new BLH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3])));
-                                break;
-                            case BLHE:
-                                returnList.add((T) new BLH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]), Double.parseDouble(splitLine[4]), Double.parseDouble(splitLine[5]), Double.parseDouble(splitLine[6])));
-                                break;
-                        }
-                    } else if (t instanceof NEH) {
-                        switch (pointType) {
-                            case NE:
-                                NEH neh_1 = new NEH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]));
-                                returnList.add((T) neh_1);
-                                break;
-                            case NEE:
-                                NEH neh_2 = new NEH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[3]));
-                                neh_2.setmN(Double.parseDouble(splitLine[2]));
-                                neh_2.setmE(Double.parseDouble(splitLine[4]));
-                                returnList.add((T) neh_2);
-                                break;
-                            case NEH:
-                                NEH neh_3 = new NEH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), Double.parseDouble(splitLine[3]));
-                                returnList.add((T) neh_3);
-                                break;
-                            case NEHE:
-                                NEH neh_4 = new NEH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[3]), Double.parseDouble(splitLine[5]));
-                                neh_4.setmN(Double.parseDouble(splitLine[2]));
-                                neh_4.setmE(Double.parseDouble(splitLine[4]));
-                                neh_4.setmH(Double.parseDouble(splitLine[6]));
-                                returnList.add((T) neh_4);
-                                break;
-                            case H:
-                                NEH neh_5 = new NEH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]));
-                                returnList.add((T) neh_5);
-                                break;
-                            case HE:
-                                NEH neh_6 = new NEH(String.valueOf(splitLine[0]), Double.parseDouble(splitLine[1]));
-                                neh_6.setmH(Double.parseDouble(splitLine[2]));
-                                returnList.add((T) neh_6);
-                                break;
-                        }
+                    } catch (NumberFormatException e) {
+                        numberFormatException.add(numberOfLineInFile);
                     }
+                } else {
+                    wrongNumberOfArgumentsInLine.put(numberOfLineInFile, splitLine.length + ";" + elementsInOneLine);
                 }
 
             }
         }
+        readFileResponse.setTotalNumberOfLinesInFile(numberOfLineInFile);
+        readFileResponse.setCorrectNumberOfLinesInFile(correctNumberOfLinesInFile - numberFormatException.size());
+        readFileResponse.setWrongNumberOfArgumentsInLine(wrongNumberOfArgumentsInLine);
+        readFileResponse.setNumberFormatExceptionSet(numberFormatException);
         return returnList;
     }
 
@@ -183,15 +210,7 @@ public class FileUtils<T extends Point> {
         return setOfDeltaHeightObservations;
     }
 
-    public List<DeltaHeight> readLevelingObservationsWithLengthOfSection(File file) throws IOException {
-        return null;
-    }
-
-    public List<T> getListT() {
-        return list;
-    }
-
-    public T getT() {
-        return t;
+    public ReadFileResponse getReadFileResponse() {
+        return readFileResponse;
     }
 }
---
+++ src/main/java/pl/sats/FileUtils/ReadFileResponse.java
@@ -0,0 +1,44 @@
+package pl.sats.FileUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ReadFileResponse {
+    private int totalNumberOfLinesInFile;
+    private int correctNumberOfLinesInFile;
+    private Map<Integer, String> wrongNumberOfArgumentsInLine;
+    private Set<Integer> numberFormatExceptionSet;
+
+    public int getTotalNumberOfLinesInFile() {
+        return totalNumberOfLinesInFile;
+    }
+
+    void setTotalNumberOfLinesInFile(int totalNumberOfLinesInFile) {
+        this.totalNumberOfLinesInFile = totalNumberOfLinesInFile;
+    }
+
+    public int getCorrectNumberOfLinesInFile() {
+        return correctNumberOfLinesInFile;
+    }
+
+    void setCorrectNumberOfLinesInFile(int correctNumberOfLinesInFile) {
+        this.correctNumberOfLinesInFile = correctNumberOfLinesInFile;
+    }
+
+    public Map<Integer, String> getWrongNumberOfArgumentsInLine() {
+        return wrongNumberOfArgumentsInLine;
+    }
+
+    void setWrongNumberOfArgumentsInLine(Map<Integer, String> wrongNumberOfArgumentsInLine) {
+        this.wrongNumberOfArgumentsInLine = wrongNumberOfArgumentsInLine;
+    }
+
+    public Set<Integer> getNumberFormatExceptionSet() {
+        return numberFormatExceptionSet;
+    }
+
+    public void setNumberFormatExceptionSet(Set<Integer> numberFormatExceptionSet) {
+        this.numberFormatExceptionSet = numberFormatExceptionSet;
+    }
+}
